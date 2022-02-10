80ed5ecdf9cba37d8888e4c268c85c038bed757b
WesleyMR77 <wesleymonare@hotmail.com>
2022-02-09 22:26:36 -0400

    JS praticamente pronto

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //LIVES CONVENCIONAIS
 function liveConventional(){
     const name = document.getElementById("name-conventional").value;
-    const checkCelebration = document.getElementById("checkCelebration-conventional").checked;
-    const checkCelebrationLink = document.getElementById("checkCelebrationLink-conventional").checked;
+    const checkCelebration = document.getElementById("check-celebration-conventional").checked;
+    const checkCelebrationLink = document.getElementById("check-celebrationlink-conventional").checked;
     const date = getDate();
 
     if(checkCelebration === true ){
@@ -43,27 +43,54 @@ function liveSeries(){
     if(checkSeriesCut === true){
         result = ("SÉRIE " + serie + " - " + message + " | " +  name)
     }
-    
-
-
     getResult(result)
 }
 
 //LIVES PERSONALIZADA
 function liveCustomizing(){
-    // const name = document.getElementById("name-customizing").value
-    // const name = document.getElementById("name-customizing").value
-    // const name = document.getElementById("name-customizing").value
-    // const name = document.getElementById("name-customizing").value
-    // const name = document.getElementById("name-customizing").value
-    
+    const name = document.getElementById("name-customizing").value;
+    const message = document.getElementById("message-customizing").value;
+    const checkCelebration = document.getElementById("check-celebration-customizing").checked
+    const checkCelebrationLink = document.getElementById("check-celebrationlink-customizing").checked
+    const supper = document.getElementById("check-supper-customizing").checked 
+    const baptism = document.getElementById("check-baptism-customizing").checked
+    const conference = document.getElementById("check-conference-customizing").checked
+    const christmas = document.getElementById("check-christmas-customizing").checked
+    const time18 = document.getElementById("check-18-customizing").checked
+    const time19 = document.getElementById("check-19-customizing").checked
 
-    // CELEBRAÇÃO IBNA - CEIA DO SENHOR | 09/01/2022 - 18H | PR. REINALDO DE SOUZA
-    // CANTATA DE NATAL IBNA 2021 | NATAL ESPERANÇA | 19/12/2021 - 19H
-    //CELEBRAÇÃO IBNA - BATISMO NAS ÁGUAS E CEIA DO SENHOR | 21/11/2021 - 18H
-    //SÉRIE FOCO FORÇA E FÉ - OS MEIOS JUSTIFICAM OS FINS | PR. ISRAEL ALCANTARA
-    //SÉRIE FOCO FORÇA E FÉ | 06/11/2021 - 19H | PR. ISRAEL ALCANTARA
-    //EM OBRAS - SÉRIE TRATAMENTO DE CHOQUE - PR ISRAEL | CELEBRA LINK
+    const dateY = new Date();
+    const year = dateY.getFullYear();
+    const date = getDate();
+    
+    if( time18 === true){
+        time = "18H"
+    }
+    if( time19 === true){
+        time = "19H"
+    }
+
+    if( checkCelebration === true ){
+        if(supper === true){
+            if( baptism === true){
+                result = ("CELEBRAÇÃO IBNA - BATISMO NAS ÁGUAS E CEIA DO SENHOR | " + date + " - " + time);
+            }
+            if( baptism === false){
+                result = ("CELEBRAÇÃO IBNA - CEIA DO SENHOR | " + date + " - " + time)                
+            }
+        }
+        if( christmas === true){
+            result = ("CANTATA DE NATAL IBNA + " + year + " | " + message +" | "+ message + " - " + time)  
+        }
+
+
+    }
+    if(checkCelebrationLink === true){
+        if(conference === true){
+            result = (message + " - " + "CONFERÊNCIA LINK " + year + " | " + name)            
+        }
+    }
+    getResult(result)
 }
 
 
@@ -80,7 +107,7 @@ function getDate(){
     const year = date.getFullYear();
 
     currentDate = day + '/' + month + '/' + year;
-    return currentDate
+    return currentDate 
 }
 
 
